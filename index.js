f10c3e31ecfcd32b3e4baa3390d8e79aca8ac832
Satyam Choudhary <Satyamchoudhary0007@gmail.com>
2023-12-18 20:24:49 +0530

    partials and navbar setup

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 require("dotenv").config();
 const configs = require("./configuartions/config");
+const path= require('node:path')
 const app = express();
 const PORT_NUMBER = configs.PORT_NUMBER || 4600;
 
+app.set('view engine','ejs')
+app.set('views',path.resolve('./views'))
 
 app.listen(PORT_NUMBER, () => {
-  console.log(`URL of App is https://localhost:${PORT_NUMBER}`);
+  console.log(`URL of App is http://localhost:${PORT_NUMBER}`);
 });
+
+app.get('/',(req,res)=>{
+    res.render('home')
+})
